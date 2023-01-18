e94f85c8d68704d56dd473474a686fee22d810f3
“Srujan03076” <srujanmahaja5@gmail.com>
2023-01-18 19:37:16 +0530

    [Srujan]Added: first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+//import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input';
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { SiginComponent } from './Components/sigin/sigin.component';
+import { RegistrationComponent } from './Components/registration/registration.component';
+import { ForgotemailComponent } from './Components/forgotemail/forgotemail.component';
+import { ForgotpasswordComponent } from './Components/forgotpassword/forgotpassword.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiginComponent,
+    RegistrationComponent,
+    ForgotemailComponent,
+    ForgotpasswordComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+   // MatCheckboxModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
