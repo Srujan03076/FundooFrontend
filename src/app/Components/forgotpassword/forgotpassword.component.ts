7c42534de5ec7b64b3ac4f84672350a5fa3c60a5
“Srujan03076” <srujanmahaja5@gmail.com>
2023-01-23 10:34:24 +0530

    [Srujan]Added: Intergrated Api for forgotemail,registration,sigin,forgotpassword components

--- src/app/Components/forgotpassword/forgotpassword.component.ts
+++ src/app/Components/forgotpassword/forgotpassword.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Token } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from 'src/app/Services/userservice/user.service';
 
 @Component({
   selector: 'app-forgotpassword',
   templateUrl: './forgotpassword.component.html',
   styleUrls: ['./forgotpassword.component.scss']
 })
-export class ForgotpasswordComponent {
+export class ForgotpasswordComponent implements OnInit{
+  
+  resetPasswordForm!: FormGroup;
+  token:any;
 
+  constructor(private formbuilder:FormBuilder, private userService :UserService,private activatedroute:ActivatedRoute ){}
+  ngOnInit(): void {
+    this. resetPasswordForm!=this.formbuilder.group({
+      password:['', [Validators.required, Validators.minLength(6)]],
+      confirmpassword:['', [Validators.required, Validators.minLength(6)]]
+    })
+    this.token =this.activatedroute.snapshot.paramMap.get('token');
+    console.log(this.token);
+  }
+  Resetpasswordsubmission(){
+    let reqdata ={
+      password:this.resetPasswordForm.value. password,
+      confirmpassword:this.resetPasswordForm.value.confirmpassword
+
+    }
+    this.userService.userResetPassword(reqdata,this.token).subscribe((response :any) =>{
+      console.log(response);
+    })
+  }
 }
--- src/app/Components/sigin/sigin.component.ts
+++ src/app/Components/sigin/sigin.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { UserService } from 'src/app/Services/userservice/user.service';
 
 @Component({
   selector: 'app-sigin',
   templateUrl: './sigin.component.html',
   styleUrls: ['./sigin.component.scss']
 })
-export class SiginComponent {
+export class SiginComponent implements OnInit{
 
+  signinForm!: FormGroup;
+  constructor(private formbuilder:FormBuilder, private userService :UserService ,private snack :MatSnackBar ){}
+
+  ngOnInit(): void {
+    this.signinForm=this.formbuilder.group({
+      emailId:['',[Validators.required,Validators.email]],
+      password:['',(Validators.required)]
+    })
+    
+  }
+  LoginSubmit(){
+    let reqdata ={
+     emailId:this.signinForm.value.emailId,
+     password:this.signinForm.value.password
+
+    }
+    this.userService.userSignin(reqdata).subscribe((response :any) =>{
+    console.log(response);
+    this.snack.open('login succesful...', '',{
+      duration: 2000,
+      verticalPosition:'bottom'
+    });
+  });
 }
+
+}  
+
